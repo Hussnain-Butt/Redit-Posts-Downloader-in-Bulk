43f5e53d30019c8f29d44ef4a505e969a542cbf0
Hussnain Butt <121555081+Hussnain-Butt@users.noreply.github.com>
2025-02-02 20:19:38 +0500

    Update app.js

--- server/app.js
+++ server/app.js
@@ -5,11 +5,23 @@ const scraperController = require("./controllers/scraperController");
 
 const app = express();
 app.use(express.json());
+const allowedOrigins = [
+    "http://172.234.216.74",
+    "http://localhost:5173"
+];
+
 app.use(cors({
-    origin: ["http://172.234.216.74"], // Sirf is frontend ko allow karega
+    origin: function (origin, callback) {
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error("Not allowed by CORS"));
+        }
+    },
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"]
-})); // Allow requests from the frontend
+}));
+
 
 // Serve static files for ZIP downloads
 app.use("/downloads", express.static(path.join(__dirname, "downloads")));
